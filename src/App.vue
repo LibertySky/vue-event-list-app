cedf7adf34c94ae24ab683882766436ea243d184
LibertySky <hello@LibertySkygraphics.com>
2021-01-26 09:26:09 -0400

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'event-list' }">All Events</router-link> |
+      <router-link :to="{ name: 'event-create' }">Add Event</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Show event #1</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
